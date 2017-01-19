7a6dce70ebea6fbe2684574b19168cbb19d4e3e3
385964 <335200woaini>
2017-01-19 10:28:36 +0800

    Added 重构工程

---
+++ module/src/main/java/com/lfh/custom/common/util/DimenUtil.java
@@ -0,0 +1,69 @@
+package com.lfh.custom.common.util;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.support.annotation.DimenRes;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+
+/**
+ * dp、sp、px间的转化
+ * Created by lifuhai on 2017/1/19 0019.
+ */
+public class DimenUtil {
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     *
+     * @param pContext {@link android.content.Context}
+     * @param pDpValue dp值
+     * @return px值
+     */
+    public static int dip2px(Context pContext, float pDpValue) {
+        DisplayMetrics metrics = pContext.getResources().getDisplayMetrics();
+        float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, pDpValue, metrics);
+
+        return (int) px;
+    }
+
+
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     *
+     * @param pContext {@link android.content.Context}
+     * @param pDpRes   dp resource
+     * @return px值
+     */
+    public static int dip2px(Context pContext, @DimenRes int pDpRes) {
+        Resources r = pContext.getResources();
+
+        return (int) r.getDimension(pDpRes);
+    }
+
+    /**
+     * 根据手机的分辨率从 px 的单位 转成为 dp
+     *
+     * @param pContext {@link android.content.Context}
+     * @param pPxValue px 值
+     * @return dip值
+     */
+    public static int px2dip(Context pContext, float pPxValue) {
+        final float scale = pContext.getResources().getDisplayMetrics().density;
+
+        return (int) (pPxValue / scale + 0.5f);
+    }
+
+
+    /**
+     * 根据手机的分辨率从 sp 的单位 转成为 px(像素)
+     *
+     * @param pContext {@link android.content.Context}
+     * @param pSpValue sp值
+     * @return px值
+     */
+    public static int sp2px(Context pContext, float pSpValue) {
+        DisplayMetrics metrics = pContext.getResources().getDisplayMetrics();
+        float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, pSpValue, metrics);
+
+        return (int) px;
+    }
+}
---
+++ module/src/main/java/com/lfh/custom/common/util/ScreenUtil.java
@@ -0,0 +1,69 @@
+package com.lfh.custom.common.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.view.ContextThemeWrapper;
+
+import java.lang.reflect.Field;
+
+/**
+ * 获取状态高度、屏幕高度、屏幕宽度
+ * Created by lifuhai on 2017/1/19 0019.
+ */
+public class ScreenUtil {
+    /**
+     * 获取状态栏高度
+     *
+     * @param pActivity activity
+     * @return 状态栏高度
+     */
+    public static int getStatusBarHeight(Activity pActivity) {
+        int statusBarHeight = 0;
+        try {
+            Class<?> c = Class.forName("com.android.internal.R$dimen");
+            Object obj = c.newInstance();
+            Field field = c.getField("status_bar_height");
+            int x = Integer.parseInt(field.get(obj).toString());
+            statusBarHeight = pActivity.getResources().getDimensionPixelSize(x);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return statusBarHeight;
+    }
+
+    /**
+     * 获取ActionBar高度
+     * @param pWrapper {@link android.view.ContextThemeWrapper}
+     * @return ActionBar高度
+     */
+    public static int getActionBarSize(ContextThemeWrapper pWrapper) {
+        int[] actionBarSizeAttr = new int[]{android.R.attr.actionBarSize};
+        TypedArray a = pWrapper.getTheme().obtainStyledAttributes(actionBarSizeAttr);
+        int actionBarSize = a.getDimensionPixelSize(0, -1);
+        a.recycle();
+
+        return actionBarSize;
+    }
+
+    /**
+     * 屏幕高度
+     *
+     * @param pContext context
+     * @return 屏幕高度
+     */
+    public static int getScreenHeight(Context pContext) {
+        return pContext.getResources().getDisplayMetrics().heightPixels;
+    }
+
+    /**
+     * 屏幕宽度
+     *
+     * @param pContext context
+     * @return 屏幕宽度
+     */
+    public static int getScreenWidth(Context pContext) {
+        return pContext.getResources().getDisplayMetrics().widthPixels;
+    }
+}
