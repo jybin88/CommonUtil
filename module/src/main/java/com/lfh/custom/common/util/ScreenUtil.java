4bed3f379c3552c0909eec583a89cbeadc1b4511
李福海 <jybin88@163.com>
2018-06-04 14:11:41 +0800

    Changed 增加获取虚拟功能键高度方法

--- module/src/main/java/com/lfh/custom/common/util/ScreenUtil.java
+++ module/src/main/java/com/lfh/custom/common/util/ScreenUtil.java
@@ -3,14 +3,21 @@ package com.lfh.custom.common.util;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Rect;
+import android.util.DisplayMetrics;
 import android.view.ContextThemeWrapper;
+import android.view.Display;
+import android.view.Window;
+import android.view.WindowManager;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 /**
  * 获取状态高度、屏幕高度、屏幕宽度
  * Created by lifuhai on 2017/1/19 0019.
  */
+@SuppressWarnings("unchecked")
 public class ScreenUtil {
 
     public ScreenUtil() {
@@ -20,17 +27,17 @@ public class ScreenUtil {
     /**
      * 获取状态栏高度
      *
-     * @param pActivity activity
+     * @param pContext 上下文
      * @return 状态栏高度
      */
-    public static int getStatusBarHeight(Activity pActivity) {
+    public static int getStatusBarHeight(Context pContext) {
         int statusBarHeight = 0;
         try {
             Class<?> c = Class.forName("com.android.internal.R$dimen");
             Object obj = c.newInstance();
             Field field = c.getField("status_bar_height");
             int x = Integer.parseInt(field.get(obj).toString());
-            statusBarHeight = pActivity.getResources().getDimensionPixelSize(x);
+            statusBarHeight = pContext.getResources().getDimensionPixelSize(x);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -40,6 +47,7 @@ public class ScreenUtil {
 
     /**
      * 获取ActionBar高度
+     *
      * @param pWrapper {@link android.view.ContextThemeWrapper}
      * @return ActionBar高度
      */
@@ -70,5 +78,50 @@ public class ScreenUtil {
      */
     public static int getScreenWidth(Context pContext) {
         return pContext.getResources().getDisplayMetrics().widthPixels;
+    }
+
+    /**
+     * 获取虚拟功能键高度
+     *
+     * @param pContext 上下文
+     * @return 虚拟功能键高度
+     */
+    public static int getVirtualBarHeight(Context pContext) {
+        int virtualBarHeight = 0;
+        WindowManager windowManager = (WindowManager) pContext.getSystemService(Context.WINDOW_SERVICE);
+
+        if (null == windowManager) {
+            return 0;
+        }
+
+        Display display = windowManager.getDefaultDisplay();
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+
+        try {
+            Class clazz = Class.forName("android.view.Display");
+            Method method = clazz.getMethod("getRealMetrics", DisplayMetrics.class);
+            method.invoke(display, displayMetrics);
+            virtualBarHeight = displayMetrics.heightPixels - windowManager.getDefaultDisplay().getHeight();
+        } catch (Exception pE) {
+            pE.printStackTrace();
+        }
+
+        return virtualBarHeight;
+    }
+
+    /**
+     * 获取虚拟功能键高度
+     *
+     * @param pActivity Activity
+     * @return 虚拟功能键高度
+     */
+    public static int getVirtualBarHeight(Activity pActivity) {
+        int titleHeight = 0;
+        Rect frame = new Rect();
+        pActivity.getWindow().getDecorView().getWindowVisibleDisplayFrame(frame);
+        int statusHeight = frame.top;
+        titleHeight = pActivity.getWindow().findViewById(Window.ID_ANDROID_CONTENT).getTop() - statusHeight;
+
+        return titleHeight;
     }
 }
